74597fe7ef3874b742151c99d32dc2571b0bba8a
EC2 Default User <ec2-user@ip-172-31-64-222.ec2.internal>
2020-09-27 02:33:40 +0000

    final polishing of project, appended to readme and added comments to the python file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import flask
 import random
 import requests
 
+# function to return a list of just ingredients and strips the rest of the information
 def getIngredientList (extendedIngredients):
     ingredientList = []
     for ingredient in extendedIngredients:
@@ -12,41 +13,49 @@ def getIngredientList (extendedIngredients):
 
     return ingredientList
 
+# key declaration
 consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
 consumer_secret = os.environ.get("TWITTER_CONSUMER_SECRET")
 access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 spoonacular_key = os.environ.get("SPOONACULAR_KEY")
 
+# twitter api authentication and access
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 twitter_api = tweepy.API(auth, wait_on_rate_limit=True)
 
+# a list of all the desserts that are parsed
 recipeNames = set(open('dishes.txt').read().split("\n"))
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
+    # generate a random dish from the list, generate the url, get the result and the id
     currDish = random.choice(tuple(recipeNames))
     urlStr = "https://api.spoonacular.com/recipes/complexSearch?query="+currDish+"&apiKey="+spoonacular_key
     content = requests.get(urlStr)
     content_json = content.json()
     random_recipe = random.choice(tuple(content_json['results']))['id']
     
+    # retrieve more detailed information about the recipe
     infoUrl = "https://api.spoonacular.com/recipes/"+str(random_recipe)+"/information?apiKey="+spoonacular_key
     print(infoUrl)
     infoContent = requests.get(infoUrl).json()
     ingredients = infoContent['extendedIngredients']
     parsedIngredients = getIngredientList(ingredients)
     
+    # search twitter for the latest 5 tweets concerning a dish
     tweets = tweepy.Cursor(twitter_api.search,
                        q=f"{currDish} -filter:retweets",
                        tweet_mode='extended',
                        lang="en").items(5)
     
+    # choose a random dish from the list of tweets
     currTweet = random.choice(list(tweets))
-
+    
+    # send information to the html to be rendered
     return flask.render_template(
         "index.html",
         currentDish = currDish,
